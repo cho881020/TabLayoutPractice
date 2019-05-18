ccfac399eb3664adbf1ca72a825c223a9f2505fe
Kyung Jin Cho <kj_cho@idealidea.co.kr>
2019-05-18 11:18:34 +0900

    커스텀 탭뷰 마무리

--- app/src/main/java/com/tje/tablayoutpractice/MainActivity.java
+++ app/src/main/java/com/tje/tablayoutpractice/MainActivity.java
@@ -3,6 +3,9 @@ package com.tje.tablayoutpractice;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 
 import com.tje.tablayoutpractice.databinding.ActivityMainBinding;
 
@@ -29,11 +32,17 @@ public class MainActivity extends BaseActivity {
         act.tabLayout.addTab(act.tabLayout.newTab().setText("채팅목록"));
         act.tabLayout.addTab(act.tabLayout.newTab().setText("검색"));
         act.tabLayout.addTab(act.tabLayout.newTab().setText("더보기"));
-        act.tabLayout.addTab(act.tabLayout.newTab().setText("더보기"));
-        act.tabLayout.addTab(act.tabLayout.newTab().setText("더보기"));
-        act.tabLayout.addTab(act.tabLayout.newTab().setText("더보기"));
-        act.tabLayout.addTab(act.tabLayout.newTab().setText("더보기"));
-        act.tabLayout.addTab(act.tabLayout.newTab().setText("더보기"));
+        act.tabLayout.addTab(act.tabLayout.newTab().setCustomView(createCustomTabView("커스텀")));
+        act.tabLayout.addTab(act.tabLayout.newTab().setCustomView(createCustomTabView("커스텀2")));
+    }
+
+    View createCustomTabView(String tabName) {
+        View tabView = LayoutInflater.from(mContext).inflate(R.layout.custom_tab, null);
+
+        TextView nameTxt = tabView.findViewById(R.id.nameTxt);
+        nameTxt.setText(tabName);
+
+        return  tabView;
     }
 
     @Override
